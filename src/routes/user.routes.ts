7c6a6cccb7591c47437d02b9f57ce314113d18ba
Bruno Lopes <brunofabricio14@hotmail.com>
2022-01-24 21:13:17 -0300

    Adcionando rotas para User

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,5 +1,4 @@
 import { request, response, Router } from "express";
-import { Iuser } from "../model/user";
 import { PrismaClient } from "@prisma/client";
 
 const userRoutes = Router();
@@ -45,6 +44,84 @@ userRoutes.post("/user", async (request, response) => {
   }
 });
 
+userRoutes.get("/users", async (request, response) => {
+  try {
+  
+    const users = prisma.user.findMany()
+
+    return response.json(users);
+ 
+  } catch (error) {
+      return response.json({message: "Try again later"});
+  }
+});
+
+userRoutes.put("user", async (request, response) => {
+  
+  try {
+    const { id } = request.headers;
+
+  const {
+    name,
+    cpf,
+    email,
+    city,
+    uf,
+    sex,
+    created_at,
+    tickets_user } = request.body;
+  
+  let user = await prisma.user.findUnique({
+    where: {
+      id: Number(id)
+    }
+  });
+
+  if(!user) {
+    return response.json({message: "User not found"});
+  }
+
+  user = await prisma.user.update({
+    where: {id: Number(id)}, 
+    data: {
+      name,
+    cpf,
+    email,
+    city,
+    uf,
+    sex,
+    created_at,
+    tickets_user
+    }
+  });
+
+  return response.json({user});
+  
+  } catch (error) {
+      
+      return response.json({message: "Try again later"});
+  }
+  
+ });
+
+userRoutes.delete("/user", async (request, response) => {
+  try {
+    const { id } = request.headers;
+
+  const user = await prisma.user.findUnique({where: {id: Number(id)}});
+
+  if(!id) {
+    return response.json({message: "User not found"});
+  }
+
+  await prisma.user.delete({where: {id: Number(id)}});
+
+  return response.json({message: "User deleted"});
+
+  } catch (error) {
+    return response.json({message: "Try again later"});
+  }
+});
 
 
 export { userRoutes };
